53a6e822af5ae0c70c60f77a2bfcd98668f700b3
Andrzej Mazur <andrzej.mazur@end3r.com>
2024-02-22 09:52:39 +0100

    G4 update

--- src/start.js
+++ src/start.js
@@ -19,14 +19,7 @@ game = new Phaser.Game(gameConfig);
 window.focus();
 
 // Usage tracking
-var head = document.getElementsByTagName('head')[0];
-var script = document.createElement('script');
-script.type = 'text/javascript';
-script.onload = function() {
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'UA-17874504-3');
-}
-script.src = 'https://www.googletagmanager.com/gtag/js?id=UA-17874504-3';
-head.appendChild(script);
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-WCX5YHRP3N');
